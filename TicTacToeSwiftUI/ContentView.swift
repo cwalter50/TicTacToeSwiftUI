b0f784944def7feeee69cd4435bc93d8767b38aa
Chris Walter <walt50@gmail.com>
2021-07-22 16:03:35 -0400

    can alternate x and o

--- TicTacToeSwiftUI/ContentView.swift
+++ TicTacToeSwiftUI/ContentView.swift
@@ -10,10 +10,37 @@ import CoreData
 
 struct ContentView: View {
     
-
+    @State private var moves = Array(repeating: "", count: 9)
+    @State private var xTurn = true
+    
     var body: some View {
-       Text("Hello, World!")
-        .padding()
+        VStack {
+            Text("Tic Tac Toe")
+                .font(.title)
+                .bold()
+            LazyVGrid(columns: Array(repeating: GridItem(.fixed(120), spacing: 15), count: 3), spacing: 15, content: {
+                ForEach(0..<9) { index in
+                    ZStack {
+                        Color.blue
+                        Text(moves[index])
+                            .font(.system(size: 90))
+                            .fontWeight(.heavy)
+                    }
+                    .frame(width: 120, height: 120, alignment: .center)
+                    .cornerRadius(30)
+                    .onTapGesture {
+                        if moves[index] == "" {
+                            moves[index] = xTurn ? "X":"O"
+                            xTurn.toggle()
+                        }
+                        
+                    }
+
+                }
+
+            })
+        }
+        .preferredColorScheme(.dark)
     }
 
     
