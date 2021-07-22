e51f52a76be84e5292613d5220035c7a57cc49d2
Chris Walter <walt50@gmail.com>
2021-07-22 16:33:35 -0400

    MVP!

--- TicTacToeSwiftUI/ContentView.swift
+++ TicTacToeSwiftUI/ContentView.swift
@@ -12,6 +12,10 @@ struct ContentView: View {
     
     @State private var moves = Array(repeating: "", count: 9)
     @State private var xTurn = true
+    @State private var gameOver = false
+    @State private var winMessage = ""
+    
+    
     
     var body: some View {
         VStack {
@@ -40,19 +44,49 @@ struct ContentView: View {
                     }
                     .rotation3DEffect(
                         .init(degrees: moves[index] != "" ? 180:0),
-                        axis: (x:   2.0, y: 1.0, z: 9.0)
+                        axis: (x:   1.0, y: 1.0, z: 9.0)
                     )
-
                 }
-
+                
             })
         }
         .preferredColorScheme(.dark)
+        .alert(isPresented: $gameOver, content: {
+            Alert(title: Text(winMessage), dismissButton: .destructive(Text("Play Again?"), action: {
+                withAnimation(Animation.default) {
+                    moves = Array(repeating: "", count: 9)
+                    gameOver = false
+                }
+            }))
+        })
+        .onChange(of: moves, perform: { value in
+            checkForWinner()
+        })
     }
-
     
     
-
+    private func checkForWinner()
+    {
+        checkLine(a: 0, b: 1, c: 2) // check top row
+        checkLine(a: 3, b: 4, c: 5) // check middle row
+        checkLine(a: 6, b: 7, c: 8) // check bottom row
+        checkLine(a: 0, b: 3, c: 6) // check left column
+        checkLine(a: 1, b: 4, c: 7) // check middle column
+        checkLine(a: 2, b: 5, c: 8) // check right column
+        checkLine(a: 0, b: 4, c: 8) // diagonal
+        checkLine(a: 2, b: 4, c: 6) // diagonal
+        
+    }
+    
+    private func checkLine(a: Int, b: Int, c: Int)
+    {
+        if moves[a] != "" && moves[a] == moves[b] && moves[b] == moves[c] {
+            winMessage = "\(moves[a]) is the winner!"
+            gameOver = true
+        }
+    }
+    
+    
     
 }
 
